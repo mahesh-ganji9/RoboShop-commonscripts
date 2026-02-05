fe13abd938455694bb26a89590563f434ca35db9
mahesh <mahesh050917@gmail.com>
2026-02-05 13:13:54 +0530

    added common.sh

---
+++ Mongodb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#This Script used to created MongoDB instance
+
+source "C:\Repos\RoboShop-commonscripts\common.sh"
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE() {
+    if [ $? -ne 0 ]; then
+     
+     echo -e "$R $2....Failure"
+    else
+     echo -e "$G $2....Success"
+     fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "mongo.repo copy process is"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "mongodb Installation is"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "enabling mongodb service is"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting mongodb service is"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
+VALIDATE $? "Replacement of /etc/mongod.conf is"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarted mongodb service is" 
