ee55037b850ffd3ee327d4af85bc9a392b13f727
mahesh <mahesh050917@gmail.com>
2026-02-05 20:42:40 +0530

    added common.sh

--- mysql.sh
+++ mysql.sh
@@ -10,10 +10,10 @@ logfolder_check
 dnf install mysql-server -y &>>$LOG_FILE
 VALIDATE $? "Installing Mysql server"
 
-systemctl enable mysqld
+systemctl enable mysqld $>>$LOG_FILE
 VALIDATE $? "enable mysql systemctl service"
 
-systemctl start mysqld  
+systemctl start mysqld  $>>$LOG_FILE
 VALIDATE $? "start systemctl mysql service"
 
 mysql_secure_installation --set-root-pass RoboShop@1
