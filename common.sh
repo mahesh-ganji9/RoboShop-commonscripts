05938883cd9583bc47d3951add41fdd6b5b1f10a
mahesh <mahesh050917@gmail.com>
2026-02-05 13:19:42 +0530

    added common.sh

---
+++ common.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+Userid=$(id -u)
+LOG_FOLDER=/var/log/ShellScript
+LOG_FILE=/var/log/ShellScript/$0.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+
+root_user_check()
+   if [ $Userid -ne 0 ]; then
+ 
+   echo -e "$Y please run the script with root access: $0"
+   exit 1
+fi
