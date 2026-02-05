f97049245c9af4137a318fe5112be8110cab2f4d
mahesh <mahesh050917@gmail.com>
2026-02-05 21:21:15 +0530

    added common.sh

--- dispatch.sh
+++ dispatch.sh
@@ -5,8 +5,14 @@ echo -e "$N This Script used for Dispatch Service Creation"
 source ./common.sh
 appname=dispatch
 
+
+
 root_user_check
 logfolder_check
 roboshop_user_check
+
+dnf install golang -y &>>$LOG_FILE
+VALIDATE $? "Installing golang is"
+
 dispatch_app
 script_execution_time 
